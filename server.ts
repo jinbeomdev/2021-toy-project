f19fa9aaffbdefe584bb79c8ac90496a06204ddf
jinbeom <niutn@naver.com>
2021-01-19 14:16:48 +0900

    Add transcoding

--- server.ts
+++ server.ts
@@ -1,13 +1,19 @@
 import * as express from 'express'
+import * as ffmpeg from 'fluent-ffmpeg'
+import { ensureDir } from 'fs-extra'
 import * as multer from 'multer'
-import { parse } from 'path'
+import { join, parse } from 'path'
 import { v4 } from 'uuid'
 import { sequelize } from './server/database'
 import { VideoModel } from './server/models/video'
+type TranscodeOptions = {
+    inputPath: string,
+    outputPath: string,
+}
 sequelize.authenticate()
 .then(() => {
     sequelize.sync()
-    console.info('database successfully connected')
+    console.info('database successfully is connected')
 })
 .catch(() => {
     process.exit(1)
@@ -28,11 +34,30 @@ function storeVideo() {
     const upload = multer({ storage })
     return upload.single('videoFile')
 } 
-function uploadVideo(req: express.Request, res: express.Response) {
-    const videoModel = new VideoModel({ uuid: parse(req.file.filename).name })
-    videoModel.save()
-    res.sendStatus(200)
-}
 app.listen(port, () => {
     console.info('express successfully started')
 })
+function uploadVideo(req: express.Request, res: express.Response) {
+    const videoModel = new VideoModel({ uuid: parse(req.file.filename).name })
+    videoModel.save()
+    transcode({
+       inputPath: req.file.path, 
+       outputPath: join('storage/hls', parse(req.file.filename).name)
+    })
+    res.sendStatus(200)
+}
+function transcode(options: TranscodeOptions) {
+    ensureDir(options.outputPath)
+    ffmpeg(options.inputPath)
+    .audioCodec('copy')
+    .videoCodec('copy')
+    .addOption(`-hls_segment_filename ${join(options.outputPath, 'out.mp4')}`)
+    .addOption('-hls_segment_type fmp4')
+    .addOption('-hls_flags single_file')
+    .addOption('-hls_playlist_type vod')
+    .output(join(options.outputPath, 'out.m3u8'))
+    .on('end', function() {
+        console.info('transcoding finished')
+    })
+    .run()
+}
