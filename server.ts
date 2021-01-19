c211910371e06ba7ac446c3e87e56eb4aa34e28f
jinbeom <niutn@naver.com>
2021-01-19 23:33:49 +0900

    Add static server for hls (m3u8, mp4)

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import * as express from 'express'
 import * as ffmpeg from 'fluent-ffmpeg'
 import { ensureDir } from 'fs-extra'
 import * as morgan from 'morgan'
-import * as cors from 'cors'
 import * as multer from 'multer'
 import { join, parse } from 'path'
 import { v4 } from 'uuid'
@@ -23,12 +22,6 @@ sequelize.authenticate()
 const app = express()
 const port = 3000
 app.use(morgan('combined'))
-app.use(cors({
-    origin: function(origin, callback) {
-        console.debug(origin)
-        callback(null)
-    }
-}))
 const videoStorer = storeVideo()
 app.post('/api/v1/video/upload', videoStorer, uploadVideo)
 function storeVideo() {
@@ -43,12 +36,7 @@ function storeVideo() {
     const upload = multer({ storage })
     return upload.single('videoFile')
 }
-app.get('/api/v1/video/watch', (req: express.Request, res: express.Response) => {
-    res.download('/Users/jinbeom/2021-toy-project/storage/hls/323fccf6-b087-4c0b-a784-e3dbcc198846/out.m3u8')
-})
-app.get('/api/v1/video/out.mp4', (req: express.Request, res: express.Response) => {
-    res.download('/Users/jinbeom/2021-toy-project/storage/hls/323fccf6-b087-4c0b-a784-e3dbcc198846/out.mp4')
-})
+app.use('/static/hls', express.static('storage/hls', { fallthrough: false }))
 app.listen(port, () => {
     console.info('express successfully started')
 })
